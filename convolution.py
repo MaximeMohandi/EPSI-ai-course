d064eb56b1227745ef0cbdda17aa395b9eab0ec5
Maxime Mohandi <mohandi.maxime@gmail.com>
2020-07-09 18:36:25 +0200

    fix : matplotlib wrong import

--- convolution.py
+++ convolution.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
